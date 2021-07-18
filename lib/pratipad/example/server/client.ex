707ca8fe6dbaa18eee622c757308d288ac2b31c6
Kentaro Kuribayashi <kentarok@gmail.com>
2021-07-19 01:15:12 +0900

    send backward message if conditions match

--- lib/pratipad/example/server/client.ex
+++ lib/pratipad/example/server/client.ex
@@ -11,15 +11,22 @@ defmodule Pratipad.Example.Server.Client do
   def backward_message(message) do
     Logger.info("backward_message: #{inspect(message)}")
 
+    percipitation = 100 # TODO
+
     data = %Pratipad.Example.Server.SensorData{
       co2_concentration: message.data.co2_concentration,
       humidity_rh: message.data.humidity_rh,
       pressure_pa: message.data.pressure_pa,
       temperature_c: message.data.temperature_c,
-      percipitation: 100
+      percipitation: percipitation
     }
     Pratipad.Example.Server.Repo.insert(data)
 
-    GenServer.cast(self(), :push_message)
+    # Pushes a message if the conditions below meet.
+    # The conditions mean the air is undesirable and you can open the door
+    # because the rain outside is not so heavy.
+    if message.data.co2_concentration > 500 && percipitation < 50 do
+      GenServer.cast(self(), :push_message)
+    end
   end
 end
