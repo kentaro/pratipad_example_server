d31aca82fdc2dd9fa48c1c3379bba46ebce2911f
Kentaro Kuribayashi <kentarok@gmail.com>
2021-07-24 00:56:57 +0900

    update epmdless

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Pratipad.Example.Server.MixProject do
       {:vega_lite, "~> 0.1.0"},
       {:kino, "~> 0.2.1"},
       {:jason, "~> 1.0"},
-      {:epmdless, "~> 0.2.0"},
+      {:epmdless, github: "tsloughter/epmdless"},
       {:distillery, "~> 2.1.1"}
     ]
   end
